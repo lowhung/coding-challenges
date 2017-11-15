3ceb38bde0b7f705db5a3dc3fa6dac5207a4bf55
hounslow <hounslowmatthew@gmail.com>
2017-11-15 07:58:19 -0800

    More problems done

--- project-euler/pythagorean-triplet.js
+++ project-euler/pythagorean-triplet.js
@@ -4,3 +4,23 @@
 //
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
+function pythagoreanTriplet(goal){
+  for (let i = 1; i < goal; i++){
+    for (let j = 1; j < goal; j++){
+      for (let m = 1; m < goal; m++){
+        if (i < j < m){
+          const equation = Math.pow(i, 2) + Math.pow(j, 2);
+          if (equation === Math.pow(m, 2)){
+            if (i + j + m === goal){
+              return `${i}${j}${m}`
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+
+// console.log(pythagoreanTriplet(1000));
+console.log(200*375*425);
